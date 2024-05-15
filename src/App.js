d87de5f5663bba8b4d7346fd328e98ee128157d3
yyeonzu <tooma22@naver.com>
2024-05-15 11:33:03 +0900

    🚚 프로젝트 구조 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import BranchPage from './pages/BranchPage';
+import DetailPage from './pages/DetailPage';
+import BasketPage from './pages/BasketPage';
+import LoginPage from './pages/LoginPage';
+import SearchResultPage from './pages/SearchResultPage';
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/branch/:branchName" element={<BranchPage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route paht="/search" element={<SearchResultPage />} />
+        <Route path="/basket" element={<BasketPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/BranchPage.jsx
@@ -0,0 +1,3 @@
+const BranchPage = () => {};
+
+export default BranchPage;
