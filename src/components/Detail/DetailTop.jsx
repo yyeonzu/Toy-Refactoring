950b8e4227ffda0990d8ea955ad8142d93300813
rwaeng <rwaeng0421@gmail.com>
2024-05-18 23:32:21 +0900

    💄 상세페이지 상단

---
+++ src/components/Detail/DetailTop.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const DetailTop = () => {
+    return (
+        <TopContainer>
+            
+        </TopContainer>
+    )
+}
+
+export default DetailTop;
+
+const TopContainer = styled.div`
+    display: flex;
+`
