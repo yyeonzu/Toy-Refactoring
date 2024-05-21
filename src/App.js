b451e6572988749e46ada107b4bbbf45e1a36cc3
yyeonzu <tooma22@naver.com>
2024-05-21 17:56:53 +0900

    ✨  layout 컴포넌트 생성 및 모든 페이지에 layout 삽입

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import DetailPage from './pages/DetailPage';
 import BasketPage from './pages/BasketPage';
 import LoginPage from './pages/LoginPage';
 import SearchResultPage from './pages/SearchResultPage';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/branch/:branchName" element={<BranchPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-        <Route paht="/search" element={<SearchResultPage />} />
-        <Route path="/basket" element={<BasketPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/branch/:branchName" element={<BranchPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route paht="/search" element={<SearchResultPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import NavBar from './NavBar';
+import {SearchBar} from '../searchbar/SearchBar';
+import BranchNavBar from './BranchNavBar';
+import {Outlet, useLocation, useParams} from 'react-router-dom';
+import Footer from './Footer';
+
+const Layout = () => {
+  const location = useLocation();
+  const {branchName} = useParams();
+
+  const isBranchRoute = location.pathname.includes('/branch/');
+
+  return (
+    <>
+      <Container>
+        <NavBar />
+        <SearchBar branch={branchName ? branchName : 'default'} />
+        {isBranchRoute && <BranchNavBar />}
+      </Container>
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
+
+const Container = styled.div`
+  min-width: 1280px;
+  width: 100vw;
+  max-width: 100%;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  border-bottom: 1px solid #33afe9;
+  padding-bottom: 8px;
+  margin-bottom: 36px;
+`;
