b428a6f336419e9bd5284942df4f0a67c9b3e432
Minyong Kim <ravenade88@gmail.com>
2024-05-24 19:54:28 +0900

    💄 style: 장바구니 UI 수정

--- src/pages/BasketPage.jsx
+++ src/pages/BasketPage.jsx
@@ -5,8 +5,8 @@ import BasketComponent from '../components/BasketPageComponent/BasketComponent';
 const BasketPage = () => {
   return (
     <BasketMain>
-      <BasketTitle>쇼핑 목록</BasketTitle>
       <BasketHead>
+        <BasketTitle>쇼핑 목록</BasketTitle>
         <BasketText>
           본 목록은 중고매장 이용 시 '프린트 출력'을 위한 임시보관 기능입니다. 로그인 저장이 안되며, PC/모바일 웹에서
           재확인할 수 없으니 유의하세요. 로그인하여 개인별 구매 WISH LIST에 저장을 원하시는 경우에는 '온라인 중고샵'
@@ -31,12 +31,16 @@ const BasketMain = styled.div`
   margin: 0 111px 277px 130px;
 `;
 
-const BasketHead = styled.div``;
+const BasketHead = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1199px;
+`;
 
 const BasketTitle = styled.div`
   width: 151px;
   height: 31px;
-  flex-direction: column;
+  display: flex;
   justify-content: center;
   text-align: center;
   color: rgba(0, 0, 0, 0.65);
@@ -44,7 +48,6 @@ const BasketTitle = styled.div`
   font-size: 24px;
   font-weight: 600;
   padding: 14px 0 14px 16px;
-  display: flex;
 `;
 
 const BasketText = styled.p`
