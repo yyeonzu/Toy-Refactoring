473334af0ef0df87159daf5d872c28a70b3996fb
Minyong Kim <ravenade88@gmail.com>
2024-05-31 03:36:54 +0900

    fix: 장바구니 삭제 기능

--- src/components/BasketPageComponent/BasketComponent.jsx
+++ src/components/BasketPageComponent/BasketComponent.jsx
@@ -24,7 +24,7 @@ const BasketComponent = () => {
   const deleteItems = async (cartId) => {
     try {
       console.log('cartId:', cartId);
-      const response = await axiosInstance.delete(`/carts/1`);
+      const response = await axiosInstance.delete(`/carts/${cartId}`);
       console.log('Delete response:', response);
       setItems((prevItems) => {
         console.log('Previous items:', prevItems); // 이전 상태 확인
